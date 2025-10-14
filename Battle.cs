44e8fdce450ad282f45f630b0ebbfdd3943f3c46
YW_Code <l.yeonwu0@gmail.com>
2025-10-14 21:09:08 +0900

    feat: PlayerAttack() 추가
    Dungeon class에 PlayerTurn() (플레이어 선턴을 잡아주는 메서드)와 PlayerAttack() (평타 메서드) 메서드를 추가함

--- Battle.cs
+++ Battle.cs
@@ -1,107 +1,127 @@
-namespace Text_RPG_11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Text_RPG_11;
 
-internal class Battle
-{
-    public int stage = 1;
-    
-    // 배틀 시작 후 몬스터 랜덤 등장
-            
-    // 0. 몬스터 담을 리스트 생성(이후에 추가)
-    public List<Monster> monsters = new List<Monster>();
-    public List<Monster> enemies = new List<Monster>();
-    
-    private GameManager _gameManager;
-    
-    public Battle(GameManager manager)
-    {
-        _gameManager = manager;
-    }
-
-    public void MonsterSpawn()
-    {
-        // 1. 몬스터 수 생성
-        Random random = new Random();
-        int spawnNum = random.Next(1, 5);
-        
-        // 2. 등장할 몬스터 랜덤 선택
-        for (int i = 0; i < spawnNum - 1; i++)
-        {
-            enemies.Add(monsters[random.Next(0, monsters.Count)]);
-        }
-            
-        // 3. 랜덤 선택된 몬스터 출력
-        Console.WriteLine("Battle!!");
-            
-        foreach (var enemy in enemies)
-        {
-            Console.WriteLine($"Lv. {enemy.Level} {enemy.Name}  HP {enemy.Hp}");
-        }
-    }
-    
-    public void WinCheck()
-    {
-        if (enemies.All(m => m.Hp == 0) && _gameManager.Player.HP > 0)
-        {
-            Console.WriteLine("승리");
-        }
-        else if (_gameManager.Player.HP <= 0)
-        {
-            Console.WriteLine("패배");
-        }
-        
-    }
-    
-    public void Attack()
-    {
-        // 사용자가 에너미를 공격
-        // 10퍼센트 확률로 160% 딜
-    }
-
-    public void UserSkill()
-    {
-        // 스킬을 사용해 에너미 공격
-    }
-
-    public void EnemyTurn()
-    {
-        // Enemy가 사용자 공격
-        // 10퍼센트 확률로 적중하지 않을 수 있음
-    }
-    
-    public void RewardShow()
-    {
-        // 체력이 0이 된 몬스터의 reward를 화면에 띄움
-        // 해당 reward를 플레이어에게 할당(reward 메서드 사용)
-    }
-    
-    public void LevelUp()
-    {
-        // 전투 클리어 후 사냥한 몬스터만큼 경험치 증가
-        // 몬스터를 순회하면서 체력이 0이 된 몬스터가 제공하는 exp만큼 플레이어의 경험치를 상승
-    }
-    
-    // public void UserPotion()
-    // {
-    //     // 유저 포션 사용 기능
-    //
-    //     // 프로퍼티 접근 체인 수정 필요
-    //     if (_gameManager.inventory.Potion.Count > 0)
-    //     {
-    //         GameManager.Player.Hp += GameManager.Player.Inventory.Potion.HealPower;
-    //         GameManager.Player.Inventory.Potion.Count--;
-    //         Console.WriteLine("회복을 완료했습니다.");
-    //     }
-    //     else
-    //     {
-    //         Console.WriteLine("포션이 부족합니다.");
-    //     }
-    // }
-}
 
 namespace Text_RPG_11
 {
-    internal class GameManager
+    internal class Battle
     {
+        public int stage = 1;
         
+        // 배틀 시작 후 몬스터 랜덤 등장
+                
+        // 0. 몬스터 담을 리스트 생성(이후에 추가)
+        public List<Monster> monsters = new List<Monster>();
+        public List<Monster> enemies = new List<Monster>();
+        
+        private GameManager _gameManager;
+        
+        public Battle(GameManager manager)
+        {
+            _gameManager = manager;
+        }
+
+        public void MonsterSpawn()
+        {
+            // 1. 몬스터 수 생성
+            Random random = new Random();
+            int spawnNum = random.Next(1, 5);
+            
+            // 2. 등장할 몬스터 랜덤 선택
+            for (int i = 0; i < spawnNum - 1; i++)
+            {
+                enemies.Add(monsters[random.Next(0, monsters.Count)]);
+            }
+                
+            // 3. 랜덤 선택된 몬스터 출력
+            Console.WriteLine("Battle!!");
+                
+            foreach (var enemy in enemies)
+            {
+                Console.WriteLine($"Lv. {enemy.Level} {enemy.Name}  HP {enemy.HP}");
+            }
+        }
+
+        public void MonsterInfo()
+        {
+            for(int i = 0; i < enemies.Count - 1; i++)
+                Console.WriteLine($"[{i + 1}] Lv. {enemies[i].Level} {enemies[i].Name}  HP {enemies[i].HP}");
+        }
+        
+        public void WinCheck()
+        {
+            if (enemies.All(m => m.HP == 0) && _gameManager.Player.HP > 0)
+            {
+                Console.WriteLine("승리");
+            }
+            else if (_gameManager.Player.HP <= 0)
+            {
+                Console.WriteLine("패배");
+            }
+            
+        }
+        
+        public void Attack(int enemyIndex)
+        {
+            Random percent = new Random();
+            percent.Next(1, 100);
+
+            if (percent.Next(1, 100) < 0)
+            {
+                enemies[enemyIndex].HP -= (int)Math.Round(_gameManager.Player.Attack * 1.6);
+            }
+            else
+            {
+                enemies[enemyIndex].HP -= _gameManager.Player.Attack;
+            }
+            
+            // 사용자가 에너미를 공격
+            // 10퍼센트 확률로 160% 딜
+        }
+
+        public void UserSkill()
+        {
+            // 스킬을 사용해 에너미 공격
+        }
+
+        public void EnemyTurn()
+        {
+            // Enemy가 사용자 공격
+            // 10퍼센트 확률로 적중하지 않을 수 있음
+        }
+        
+        public void RewardShow()
+        {
+            // 체력이 0이 된 몬스터의 reward를 화면에 띄움
+            // 해당 reward를 플레이어에게 할당(reward 메서드 사용)
+        }
+        
+        public void LevelUp()
+        {
+            // 전투 클리어 후 사냥한 몬스터만큼 경험치 증가
+            // 몬스터를 순회하면서 체력이 0이 된 몬스터가 제공하는 exp만큼 플레이어의 경험치를 상승
+        }
+        
+        // public void UserPotion()
+        // {
+        //     // 유저 포션 사용 기능
+        //
+        //     // 프로퍼티 접근 체인 수정 필요
+        //     if (_gameManager.inventory.Potion.Count > 0)
+        //     {
+        //         GameManager.Player.Hp += GameManager.Player.Inventory.Potion.HealPower;
+        //         GameManager.Player.Inventory.Potion.Count--;
+        //         Console.WriteLine("회복을 완료했습니다.");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("포션이 부족합니다.");
+        //     }
+        // }
     }
+
 }
--- Dungeon.cs
+++ Dungeon.cs
@@ -10,25 +10,86 @@ namespace Text_RPG
     internal class Dungeon
     {
         private GameManager _gameManager;
-        private Battle battle;
+        private Battle _battle;
         
         public Dungeon(GameManager manager)
         {
             // 배틀 시작
             _gameManager = manager;
-            battle = new Battle(manager);
-            Console.WriteLine("Battle!!");
-            battle.MonsterSpawn();
+            _battle = new Battle(manager);
+            Console.WriteLine("Battle!!\n\n");
+            _battle.MonsterSpawn();
             Console.WriteLine("[내 정보]");
             // 추후 Player 클래스 수정 후 currentHP or HpMax로 추가 예정
             Console.WriteLine($"Lv.{_gameManager.Player.Level} Chad ({_gameManager.Player.Job}) \n\n HP {_gameManager.Player.HP}");
         }
-        
+
+        // 공격 / 스킬 사용 선택
         public void PlayerTurn()
         {
+            Console.WriteLine("Battle!!\n\n");   
+            _battle.MonsterInfo();
+            
+            Console.WriteLine("[내 정보]");
+            // 추후 Player 클래스 수정 후 currentHP or HpMax로 추가 예정
+            Console.WriteLine($"Lv.{_gameManager.Player.Level} Chad ({_gameManager.Player.Job})  HP {_gameManager.Player.HP}\n\n");
+
+            bool isWorked = int.TryParse(Console.ReadLine(), out int result);
+
+            switch (result)
+            {
+                case 1:
+                    PlayerAttack();
+                    break;
+                case 2:
+                    PlayerSkill();
+                    break;
+            }
+        }
+        
+        // 공격
+        public void PlayerAttack()
+        {
+            Console.WriteLine("Battle!!\n\n");   
+            _battle.MonsterInfo();
+            
+            Console.WriteLine("[내 정보]");
+            // 추후 Player 클래스 수정 후 currentHP or HpMax로 추가 예정
+            Console.WriteLine($"Lv.{_gameManager.Player.Level} Chad ({_gameManager.Player.Job})  HP {_gameManager.Player.HP}\n\n");
+            
             // 플레이어 턴 = 공격 입력
             // 만약 1을 입력했다면 공격
             // 만약 2를 입력했다면 스킬을 보여주고 > 스킬 공격
+            
+            Console.WriteLine("몬스터 숫자. 공격\n\n");
+            Console.WriteLine("0. 취소\n\n");
+
+            Console.WriteLine("대상을 선택해주세요.");
+            Console.Write(">>");
+            
+            bool isWorked = int.TryParse(Console.ReadLine(), out int monsterNum);
+
+            if (isWorked || monsterNum == 0)
+            {
+                // 나가기
+            }
+            else if(monsterNum > 0 && monsterNum <= _battle.enemies.Count)
+            {
+                _battle.Attack(monsterNum - 1);
+            }
+
+        }
+        
+        // 스킬 선택
+        public void PlayerSkillSelect()
+        {
+            
+        }
+        
+        // 스킬 공격
+        public void PlayerSkill()
+        {
+            
         }
 
         public void EnemyTurnToPlayerTurn()
