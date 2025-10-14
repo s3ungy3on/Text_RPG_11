cf1ac0fd965d0479fba6e71d38d9eae91c8bb7bf
seungyeon <s3ungy3on@gmail.com>
2025-10-14 19:25:28 +0900

    feat: 아이템 및 인벤토리 구현

---
+++ Messages.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_RPG_11
+{
+    internal class Messages
+    {
+        public static void TextTitleHlight(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        public static void TextMagentaHlight(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+
+        public static void Equipped(bool isEquipped)
+        {
+            if (isEquipped)
+            {
+                Console.Write("[");
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.Write("E");
+                Console.ResetColor();
+                Console.Write("]");
+            }
+        }
+
+
+        public static int ReadInput(int min, int max)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                int number;
+
+                if(!int.TryParse(input, out number))
+                {
+                    Console.WriteLine("잘못된 입력입니다. 숫자를 입력해주세요.");
+                    continue;
+                }
+
+                if(number < min || number > max)
+                {
+                    Console.WriteLine($"잘못된 입력입니다. {min}~{max} 사이의 숫자를 입력해주세요.");
+                    continue;
+                }
+
+                return number;
+            }
+        }
+    }
+}
