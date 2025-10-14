d83df1553d1c8129ca678b97254b8ab0b4f235e5
seungyeon <s3ungy3on@gmail.com>
2025-10-14 16:22:14 +0900

    chore: 이그노어 프로젝트 초기화

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Text_RPG_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
