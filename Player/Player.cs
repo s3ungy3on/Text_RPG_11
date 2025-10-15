3e2576e8ff0ec96017497fdd42f42f92c5176fc3
tjdgus76 <01075723727a@gmail.com>
2025-10-15 14:33:13 +0900

    feat: 플레이어 MP 추가

--- Player/Player.cs
+++ Player/Player.cs
@@ -17,34 +17,38 @@ namespace Text_RPG_11
         public int Attack { get; set; }
         public int Defense { get; set; }
         public int HP { get; set; }
+        public int MP { get; set; }
         public int Gold { get; set; }
         public int Exp { get; set; }
 
         private int itemHP = 0;
+        private int iteMP = 0;
         public int itemAttack = 0;
         public int itemDefense = 0;
 
         public int MaxHP { get { return HP + itemHP; } }
-
+        public int MaxMP { get { return MP + itemMP; } }
         public int MaxAttack { get { return Attack + itemAttack; } }
         public int MaxDefense { get { return Defense + itemDefense; } }
 
 
-        public Player(string name, int level, string job, int attack, int defense, int hP, int gold, int exp = 0)
+        public Player(string name, int level, string job, int attack, int defense, int hp, int gold, int exp = 0, int mp = 50)
         {
             Name = name;
             Level = level;
             Job = job;
             Attack = attack;
             Defense = defense;
-            HP = hP;
+            HP = hp;
             Gold = gold;
             Exp = exp;
+            MP = mp;
         }
 
         public void StatUpdate(GameManager gameManager)
         {
             itemHP = 0;
+            itemMP = 0;
             itemAttack = 0;
             itemDefense = 0;
 
@@ -60,18 +64,27 @@ namespace Text_RPG_11
                     if (item is Weapon weapon)
                     {
                         itemAttack += weapon.AttackPower;
+
                         if(weapon.ItemHp > 0)
-                        {
                             itemHP += weapon.ItemHp;
-                        }
+
+                        if (weapon.ItemMp > 0)          
+                            itemMP += weapon.ItemMp;
+                        
+
                     }
                     else if (item is Armor armor)
                     {
                         itemDefense += armor.DefensePower;
+
                         if(armor.ItemHp > 0)
-                        {
+                        
                             itemHP += armor.ItemHp;
-                        }
+
+                        if (armor.ItemMp > 0)           
+                            itemMP += armor.ItemMp;
+
+                        
                     } 
                 }
             }
